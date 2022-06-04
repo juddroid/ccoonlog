76be0cf7a6ed8e49222cbcf8a4006cebcb371bfc
Raccoon <nobllizdc@gmail.com>
2022-06-05 05:19:53 +0900

    [temp]

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,15 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
-  framework: '@storybook/react',
-};
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,9 @@
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../src/styles/GlobalStyles';
-import light from '../src/styles/lightTheme';
-
-export const decorators = [
-  (Story) => (
-    <>
-      <ThemeProvider theme={light}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
-    </>
-  ),
-];
-
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
+}
